52b12e912c855a633991d0d12cb00ac3ef52a3af
Angkawipar Tubiam <angkawipar@gmail.com>
2022-08-06 20:54:24 +0700

    print

--- lib/game.dart
+++ lib/game.dart
@@ -3,22 +3,23 @@ import 'dart:math';
 
 class Game{
   int answer = 0;
-
-  Game(){
+  Game({int? maxRandom=100}){
     var r = Random();
-    answer = r.nextInt(100)+1;
-    print('คำตอบคือ $answer');
+    answer = r.nextInt(maxRandom!)+1;
+
   }
 
   int doGuess(int num){
     if(num>answer){
-      print('$num is Too high');
+      print('║➜ $num is Too high ▲');
+      print('╟───────────────────────────────────────────');
       return 0;
     }else if(num<answer){
-      print('$num is Too low');
+      print('║➜ $num is Too low ▼');
+      print('╟───────────────────────────────────────────');
       return 0;
     }else{
-      stdout.write('$num is Correct. ');
+      stdout.write('║➜ $num is Correct❤, ');
       return 1;
     }
   }
