b7d363ef3269882c9a324a0861ea6b94500531f0
Angkawipar Tubiam <angkawipar@gmail.com>
2022-08-10 16:19:55 +0700

    print

--- lib/hello.dart
+++ lib/hello.dart
@@ -5,71 +5,74 @@ import 'dart:math';
 import 'game.dart';
 
 void main(){
-  int result=0;
-  int conti =0;
-  List<int> myList = [];
+int result=0;
 
+int conti=0;
+List<int> myList = [];
 
   while(true){
-  int total=0;
-  print('╔═══════════════════════════════════════════');
-  print('║            🚀guess the number🚀           ');
-  print('╟───────────────────────────────────────────');
-  stdout.write("║Enter a maximum number to random: ");
-  var x = stdin.readLineSync();
-  var s = int.tryParse(x!);
-  var game = Game(maxRandom:s);
-  do{
-    stdout.write('║Please guess the number betaween 1 and $s : ');
-    var input = stdin.readLineSync();
-    var guess = int.tryParse(input!);
+    int total=0;
+    print('╔═══════════════════════════════════════════');
+    print('║            🚀guess the number🚀           ');
+    print('╟───────────────────────────────────────────');
+    stdout.write("║Enter a maximum number to random: ");
+    var x = stdin.readLineSync();
+    var s= int.tryParse(x!);
+    var game = Game(maxRandom: s);
+    do{
+      stdout.write('║Please guess the number betaween 1 and $s : ');
+      var input = stdin.readLineSync();
+      var guess = int.tryParse(input!);
 
-    if(guess == null) {
-      //print('Please enter number only');
-      continue;
-    }else{
-      total++;
-      result = game.doGuess(guess);
+
+      if(guess == null) {
+        //print('Please enter number only');
+        continue;
+      }else{
+        total++;
+        result = game.doGuess(guess);
+      }
+
+    }while(result !=1);
+    myList.add(total);
+
+    if(result ==1){
+      print('total guesses: $total');
+      print('                 The end                   ');
+      print('═══════════════════════════════════════════');
     }
-  }while(result !=1);
-  myList.add(total);
-  if(result ==1){
-    print('total guesses: $total');
-    print('              The end             ');
-    print('════════════════════════════════════════════');
-  }
-
-  stdout.write("Play again? (y/n): ");
-  var Y ="Y";
-  var y = "y";
-  var n = "n";
-  var N = "N";
-  while(true){
-    var a = stdin.readLineSync();
-    var ag = a.toString();
-    if(ag ==y || ag==Y){
+    stdout.write("Play agin? (y/n): ");
+    var Y ="Y";
+    var y = "y";
+    var n = "n";
+    var N = "N";
+    while(true){
+      var a = stdin.readLineSync();
+      var con = a.toString();
+      if(con==Y || con==y){
+        break;
+      }else if(con==N || con==n){
+        conti=1;
+        break;
+      }else{
+        stdout.write("Play agin? (y/n): ");
+        continue;
+      }
+    }
+    if(conti==1){
       break;
     }
-    else if(ag ==n || ag==N){
-      conti =1;
-      break;
-    }
-    else{
-      stdout.write('Play again? (y/n): ');
-      continue;
-    }
   }
-  if(conti ==1){
-    break;
-  }
-}
   var len = myList.length;
-  print("You've play $len game");
-  for(int i=0;i<myList.length;i++){
-    stdout.write('🚀Game #${i+1}: ');
-    stdout.write(myList[i]);
+  print("You've played $len game");
+  for (var i = 0; i < myList.length; i++) {
+    int g=i+1;
+    stdout.write("🚀Game #$g: ");
+    stdout.write( myList[i]);
     print(" guess");
   }
+
+
 
 }
 
